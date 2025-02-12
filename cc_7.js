34f07b06a42041b640a0c147cc19705b77c4a5b2
Your Name <your.email@example.com>
2025-02-12 16:27:10 -0500

    Task 1 - Customer Invoice Calculation

--- cc_7.js
+++ cc_7.js
@@ -1,5 +1,34 @@
+// Task 1: Function Declaration
+
+function calculateInvoice(subtotal, taxRate, discount) 
+{
+    let total = (subtotal + (subtotal * taxRate)- discount)
+    {
+ return (total.toFixed(2)); }
+}
+// creating a function that calculates the final invoice amount
 
 
+console.log("Customer Invoice Calculation:");
+console.log(`Invoice Amount 1: $${calculateInvoice(100, 0.08, 5)}`);
+console.log(`Invoice Amount 1: $${calculateInvoice(500, 0.1, 20)}`);
+// Logging the test data given.
+// Using a template literal to give us the output of each invoice amount
 
+// Task 2: Function Expression
+
+function calculateHourlyWage(salary, hoursPerWeek)
+{ 
+   let hourlyWage = ( salary / (hoursPerWeek * 52));
+  {  return (hourlyWage.toFixed(2));  // using .toFixed to give each output two decimal places
+}
+}
+// creating a calculation that finds the hourly wage per week based on salary and hours
+
+console.log("Hourly Wages:")
+console.log(`Hourly Wage 1: $${calculateHourlyWage(52000, 40)}`);
+console.log(`Hourly Wage 2: $${calculateHourlyWage(75000, 35)}`);
+// logging the test data give
+// Using template literal to give us the output
 
 
