af4d790155c7a4bb5bdc32ca22fab6ef23410652
Your Name <your.email@example.com>
2025-02-13 17:27:24 -0500

    Task 3 - Customer Loyalty Discount

--- cc_7.js
+++ cc_7.js
@@ -31,4 +31,21 @@ console.log(`Hourly Wage 2: $${calculateHourlyWage(75000, 35)}`);
 // logging the test data give
 // Using template literal to give us the output
 
+// Task 3: Arrow Function
 
+const calculateLoyaltyDiscount = (amount, years) => {
+// creating an arrow function that applies a discount
+    let discountRate; 
+
+    if (years >= 5) { discountRate = .15; }  // applies a 15% discount if theres over 5 years
+    else if (years >= 3) {discountRate =.10; } // applies a 10% discount is theres over 3 years
+    else if (years < 3) {discountRate = .05; } // applies a 5% discount if theres less than 3 years
+
+    return (amount - (amount * discountRate)).toFixed(2);
+// gives us the calculated discount and .toFixed is used to display two decimal places
+}
+
+console.log("Loyalty Discounts:");
+console.log(`Loyalty Discount 1: $${calculateLoyaltyDiscount(100, 6)}`); //test data 1
+console.log(`Loyalty Discount 2: $${calculateLoyaltyDiscount(200, 2)}`); // test data 2
+// logging the test data given
